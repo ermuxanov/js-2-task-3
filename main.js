ca707826abf458e017f062c7bffe230058721f0e
ermuxanov <74331498+ermuxanov@users.noreply.github.com>
2022-10-08 15:09:08 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var one = +prompt("Введите первое число");
+var two = +prompt("Введите второе число");
+var three = +prompt("Введите третье число");
+
+if (one > two && one < three) {
+  alert("Среднее число: " + one);
+} else if (two > one && two < three) {
+  alert("Среднее число: " + two);
+} else if (three > one && three < two) {
+  alert("Среднее число: " + three);
+} else if (three == one && three == two && two == one) {
+  alert("Все числа равны");
+} else if (one == two && one < three) {
+  alert("Мин. число: " + one + " и макс. число: " + three);
+} else if (one == two && one > three) {
+  alert("Мин. число: " + three + " и макс. число: " + one);
+} else if (two == three && one > three) {
+  alert("Мин. число: " + three + " и макс. число: " + one);
+} else if (two == three && one < three) {
+  alert("Мин. число: " + one + " и макс. число: " + three);
+} else if (one == three && one < two) {
+  alert("Мин. число: " + one + " и макс. число: " + two);
+} else if (one == three && one > two) {
+  alert("Мин. число: " + two + " и макс. число: " + one);
+}
