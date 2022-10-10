edf14d3381f1ead89517cd551a2a15f532029f6b
ermuxanov <74331498+ermuxanov@users.noreply.github.com>
2022-10-10 13:11:34 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ var three = +prompt("Введите третье число");
 
 if (one > two && one < three) {
   alert("Среднее число: " + one);
+} else if (one > three && one < two) {
+  alert("Среднее число: " + one);
 } else if (two > one && two < three) {
   alert("Среднее число: " + two);
+} else if (two > three && two < one) {
+  alert("Среднее число: " + two);
 } else if (three > one && three < two) {
+  alert("Среднее число: " + three);
+} else if (three > two && three < one) {
   alert("Среднее число: " + three);
 } else if (three == one && three == two && two == one) {
   alert("Все числа равны");
